1b20bc179a022dd384d2db6a36f779ab0a0e0ca2
lneninger <lneninger@hipalanet.com>
2022-11-10 10:59:18 -0500

    3 files modified

--- src/app/cdk-virtual-scroll-append-only-example.ts
+++ src/app/cdk-virtual-scroll-append-only-example.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ISmartField } from './models/items.models';
 
 /** @title Virtual scroll with view recycling disabled. */
@@ -9,37 +9,48 @@ import { ISmartField } from './models/items.models';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CdkVirtualScrollAppendOnlyExample {
-
   index: number;
+  _load: any;
+  @Input() set load(value: any) {
+    this._load = value;
+    this.getFields();
+  }
 
-  items = Array.from({length: 100000}).map((_, i) => {
+  items = Array.from({ length: 100000 }).map((_, i) => {
     return {
       id: i,
       value: `Item #${i}`,
       metadata: {
-        type: i % 1 ? 'TEXT' : (i % 2 ? 'RADIO' : 'CHECK')
-      }
+        type: i % 1 ? 'TEXT' : i % 2 ? 'RADIO' : 'CHECK',
+      },
     } as ISmartField;
     // return `Item #${i}`;
   });
-  filtered = this.items;
 
-  filter(type: 'ALL' | 'EVEN'){
-    switch(type){
-        case 'EVEN' :
-          this.filtered = this.items.filter((item, index) => index % 2);
-          break;
-          default:
-            this.filtered = this.items;
-            break;
-          }
+  filtered: ISmartField[];
+
+  getFields() {
+    this.filtered = this.items;
   }
 
-  // onIndexChange($event: number){
-  //   this.index = $event;
-  // }
-}
+  filter(type: 'ALL' | '2X' | '3X') {
+    switch (type) {
+      case '2X':
+        this.filtered = this.items.filter((item, index) => index % 2);
+        break;
+      case '3X':
+        this.filtered = this.items.filter((item, index) => index % 3);
+        break;
+      default:
+        this.filtered = this.items;
+        break;
+    }
+  }
 
+  onIndexChange($event: number) {
+    this.index = $event;
+  }
+}
 
 /**  Copyright 2022 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
